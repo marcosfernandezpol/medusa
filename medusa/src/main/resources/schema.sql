d8d569ebe55a477c3bda35958411e11d3da8e4a7
José Manuel Turnes Pazos <jose.turnes@udc.es>
2021-09-16 16:02:23 +0200

    Script sql modificado

--- medusa/src/main/resources/schema.sql
+++ medusa/src/main/resources/schema.sql
@@ -1,12 +1,48 @@
+DROP TABLE IF EXISTS Enterprise;
+DROP TABLE IF EXISTS Bank_account;
+DROP TABLE IF EXISTS Transfer_history;
 DROP TABLE IF EXISTS User;
 
 CREATE TABLE User (
-    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    userName VARCHAR(60) NOT NULL,
-    password VARCHAR(60) NOT NULL, 
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    userName VARCHAR(60) COLLATE latin1_bin NOT NULL,
+    password VARCHAR(60) NOT NULL,
     firstName VARCHAR(60) NOT NULL,
-    lastName VARCHAR(60) NOT NULL, 
-    email VARCHAR(60) NOT NULL,
+    lastName VARCHAR(60) NOT NULL,
     role TINYINT NOT NULL,
+    country VARCHAR(60) NOT NULL,
+    city VARCHAR(60) NOT NULL,
+    CONSTRAINT UserPK PRIMARY KEY (id),
+    CONSTRAINT UserNameUniqueKey UNIQUE (userName)
 );
 
+CREATE TABLE Enterprise (
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    enterpriseName VARCHAR(60) COLLATE latin1_bin NOT NULL, 
+    acronim VARCHAR(10) COLLATE latin1_bin NOT NULL,
+    fundation DATE NOT NULL,
+    incomes FLOAT NOT NULL,
+    annualBenefits FLOAT NOT NULL,
+    CONSTRAINT EnterprisePK PRIMARY KEY (id),
+    CONSTRAINT enterpriseNameUnique UNIQUE (enterpriseName)
+);
+
+CREATE TABLE  Bank_account (
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    accountNumber BIGINT NOT NULL,
+    balance FLOAT NOT NULL,
+    owner BIGINT NOT NULL,
+    CONSTRAINT Bank_accountPK PRIMARY KEY (id),
+    CONSTRAINT Bank_accountFK FOREIGN KEY (owner) REFERENCES User(id),
+    CONSTRAINT accountNumberUnique UNIQUE (accountNumber)
+);
+
+CREATE TABLE Transfer_history (
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    date DATETIME NOT NULL,
+    sender BIGINT NOT NULL,
+    receiver BIGINT NOT NULL,
+    CONSTRAINT TransferPK PRIMARY KEY (id),
+    CONSTRAINT SenderFK FOREIGN KEY (sender) REFERENCES User(id),
+    CONSTRAINT ReceiverFK FOREIGN KEY (receiver) REFERENCES User(id)
+);
