0a4499731d55b460dfed1d3db301ed93d241b054
Jose Manuel Turnes Pazos <jose.turnes@udc.es>
2021-11-09 18:08:23 +0100

    [#1362] Arreglada actualizacion del estado

--- frontend/src/modules/search/components/FindEnterpriseResult.js
+++ frontend/src/modules/search/components/FindEnterpriseResult.js
@@ -38,18 +38,19 @@ const FindEnterpriseResult = () => {
 		const enterpriseId = Number(id);
 		if (!Number.isNaN(enterpriseId)) {
 			dispatch(actions.searchEnterpriseById(enterpriseId));
+			dispatch(actions.searchEnterpriseHistoric(enterpriseId));
 		}
 
 		return () => null;
 
 	}, [id, dispatch]);
 	
-	useEffect(() => {
+	/*useEffect(() => {
 		const enterpriseId = Number(id);
 		if (!Number.isNaN(enterpriseId)) {
         	dispatch(actions.searchEnterpriseHistoric(enterpriseId));
 		}
-    }),[id, dispatch];
+    }),[id, dispatch];*/
 
 	
 	const data = useSelector(selectors.getEnterpriseHistoric);
