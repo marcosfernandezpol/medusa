04b2ec23528b964840d014c3b9c7c620b3d52158
anton <anton.gendra@udc.es>
2021-11-13 16:16:42 +0100

    Implements feature #1361 , cambiados todos los daos, entidades y servicios que tenian que ver con crear ordenes y buscar ordenes con el fin de implementar esta funcinalidad. Modificados los tests para que cuadraran con el nuevo atributo de OrderLine y creado un test para testear esta nueva caracteristica @0.75h

--- src/main/java/es/udc/fi/dc/fd/rest/dtos/OrderLineConversor.java
+++ src/main/java/es/udc/fi/dc/fd/rest/dtos/OrderLineConversor.java
@@ -11,19 +11,20 @@ public class OrderLineConversor {
 	}
 
 	public final static OrderLineDto toOrderLineDto(OrderLine order) {
-		if(order.getOwner()==null) {
-			return new OrderLineDto(order.getId(), order.getRequestDate(), order.getOrderType(), null,
-					order.getPrice(), order.getNumber(), order.getEnterprise().getId());
+		if (order.getOwner() == null) {
+			return new OrderLineDto(order.getId(), order.getRequestDate(), order.getOrderType(), null, order.getPrice(),
+					order.getNumber(), order.getEnterprise().getId(), order.getDeadline());
+		} else {
+			return new OrderLineDto(order.getId(), order.getRequestDate(), order.getOrderType(),
+					order.getOwner().getId(), order.getPrice(), order.getNumber(), order.getEnterprise().getId(),
+					order.getDeadline());
 		}
-		else {
-			return new OrderLineDto(order.getId(), order.getRequestDate(), order.getOrderType(), order.getOwner().getId(),
-					order.getPrice(), order.getNumber(), order.getEnterprise().getId());
-		}
-		
+
 	}
 
 	public final static List<OrderLineDto> toOrderLineDtos(List<OrderLine> orders) {
 		return orders.stream().map(order -> toOrderLineDto(order)).collect(Collectors.toList());
 	}
+
 
 }
